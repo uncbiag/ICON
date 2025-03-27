Change masking strategy, update naming conventions, and fix bugs

src/icon_registration/itk_wrapper.py
@@ -23,17 +23,18 @@ def finetune_execute(model, image_A, image_B, steps):
     model.load_state_dict(state_dict)
     return loss
 
+
 def finetune_execute_mask(model, image_A, image_B, mask_A, mask_B, steps):
     state_dict = copy.deepcopy(model.state_dict())
     optimizer = torch.optim.Adam(model.parameters(), lr=0.00002)
     for _ in range(steps):
         optimizer.zero_grad()
-        loss_tuple = model(image_A, image_B, mask_A, mask_B)
+        loss_tuple = model(image_A, image_B, mask_A=mask_A, mask_B=mask_B)
         print(loss_tuple)
         loss_tuple[0].backward()
         optimizer.step()
     with torch.no_grad():
-        loss =  model(image_A, image_B, mask_A, mask_B)
+        loss =  model(image_A, image_B, mask_A=mask_A, mask_B=mask_B)
     model.load_state_dict(state_dict)
     return loss
 
@@ -106,8 +107,8 @@ def register_pair_with_mask(model, image_A, image_B, mask_A, mask_B, finetune_st
     A_npy = np.array(image_A)
     B_npy = np.array(image_B)
     
-    mask_A_npy = np.array(mask_A)
-    mask_B_npy = np.array(mask_B)
+    A_mask_npy = np.array(mask_A)
+    B_mask_npy = np.array(mask_B)
 
     assert(np.max(A_npy) != np.min(A_npy))
     assert(np.max(B_npy) != np.min(B_npy))
@@ -115,8 +116,8 @@ def register_pair_with_mask(model, image_A, image_B, mask_A, mask_B, finetune_st
     # turn images into torch Tensors: add feature and batch dimensions (each of length 1)
     A_trch = torch.Tensor(A_npy).to(config.device)[None, None]
     B_trch = torch.Tensor(B_npy).to(config.device)[None, None]
-    mask_A_trch = torch.Tensor(mask_A_npy).to(config.device)[None, None]
-    mask_B_trch = torch.Tensor(mask_B_npy).to(config.device)[None, None]
+    A_mask_trch = torch.Tensor(A_mask_npy).to(config.device)[None, None]
+    B_mask_trch = torch.Tensor(B_mask_npy).to(config.device)[None, None]
 
     shape = model.identity_map.shape
 
@@ -130,11 +131,11 @@ def register_pair_with_mask(model, image_A, image_B, mask_A, mask_B, finetune_st
         B_trch, size=shape[2:], mode="trilinear", align_corners=False
     )
     
-    mask_A_resized = F.interpolate(
-        mask_A_trch, size=shape[2:], mode="nearest"
+    A_mask_resized = F.interpolate(
+        A_mask_trch, size=shape[2:], mode="nearest"
     )
-    mask_B_resized = F.interpolate(
-        mask_B_trch, size=shape[2:], mode="nearest"
+    B_mask_resized = F.interpolate(
+        B_mask_trch, size=shape[2:], mode="nearest"
     )
     
     if finetune_steps == 0:
@@ -142,9 +143,9 @@ def register_pair_with_mask(model, image_A, image_B, mask_A, mask_B, finetune_st
 
     if finetune_steps == None:
         with torch.no_grad():
-            loss = model(A_resized, B_resized, mask_A_resized, mask_B_resized)  
+            loss = model(A_resized, B_resized, mask_A=A_mask_resized, mask_B=B_mask_resized)  
     else:
-        loss = finetune_execute_mask(model, A_resized, B_resized, mask_A_resized, mask_B_resized, finetune_steps)
+        loss = finetune_execute_mask(model, A_resized, B_resized, A_mask_resized, B_mask_resized, finetune_steps)
 
     # phi_AB and phi_BA are [1, 3, H, W, D] pytorch tensors representing the forward and backward
     # maps computed by the model

src/icon_registration/losses.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 from icon_registration import config, network_wrappers
 
-#from .mermaidlite import compute_warped_image_multiNC
+from .mermaidlite import compute_warped_image_multiNC
 
 
 def to_floats(stats):
@@ -591,35 +591,37 @@ class LNCC(SimilarityBase):
     def __call__(self, image_A, image_B, mask_B=None):
         I = image_A
         J = image_B
-        assert I.shape == J.shape, "The shape of image I and J sould be the same."
+        assert I.shape == J.shape, "The shape of image I and J should be the same."
         
         if mask_B is not None:
-            assert mask_B.shape == J.shape, "The shape of mask and J sould be the same."
-            assert torch.sum(mask_B) > 0, "The mask should have nonzero values."
+            assert mask_B.shape == J.shape, "The shape of mask and J should be the same."
         
         lncc = self.calculate_lncc(I, J)
         
         if mask_B is not None:
-            lncc = lncc[mask_B.bool()]
+            mask = mask_B.clone()
+            mask[mask != 0] = 1
+            lncc = lncc * mask
 
-        return torch.mean(1 - lncc)
+        return 1 - torch.mean(lncc)
 
 class SquaredLNCC(LNCC):
     def __call__(self, image_A, image_B, mask_B=None):
         I = image_A
         J = image_B
-        assert I.shape == J.shape, "The shape of image I and J sould be the same."
+        assert I.shape == J.shape, "The shape of image I and J should be the same."
         
         if mask_B is not None:
-            assert mask_B.shape == J.shape, "The shape of mask and J sould be the same."
-            assert torch.sum(mask_B) > 0, "The mask should have nonzero values."
+            assert mask_B.shape == J.shape, "The shape of mask and J should be the same."
         
         lncc = self.calculate_lncc(I, J)
         
         if mask_B is not None:
-            lncc = lncc[mask_B.bool()]
+            mask = mask_B.clone()
+            mask[mask != 0] = 1
+            lncc = lncc * mask
             
-        return torch.mean(1 - lncc**2)
+        return 1 - torch.mean(lncc**2)
 
 class LNCCOnlyInterpolated(SimilarityBase):
     def __init__(self, sigma):
@@ -810,16 +812,18 @@ class MINDSSC(SimilarityBase):
         return mind
 
     def __call__(self, image_A, image_B, mask_B=None):
-        assert image_A.shape == image_B.shape, "The shape of image_A and image_B sould be the same."
+        assert image_A.shape == image_B.shape, "The shape of image_A and image_B should be the same."
         assert len(image_A.shape) - 2 == 3, "The input image should be 3D."
         
         difference = self.compute_mindssc(image_A) - self.compute_mindssc(image_B)
         
         if mask_B is not None:
-            assert mask_B.shape == image_B.shape, "The shape of mask and image_B sould be the same."
-            assert torch.sum(mask_B) > 0, "The mask should have nonzero values."
-            mask_B = mask_B.expand(-1, difference.shape[1], -1, -1, -1)
-            difference = difference[mask_B.bool()]
+            assert mask_B.shape == image_B.shape, "The shape of mask and image_B should be the same."
+            mask = mask_B.clone()
+            mask[mask != 0] = 1
+
+            mask = mask.expand(-1, difference.shape[1], -1, -1, -1)
+            difference = difference * mask
             
         return torch.mean(difference ** 2)
 